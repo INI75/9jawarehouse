9d7033575ed1217f294317f25382bcd0c87a9fab
INI75 <isethugbosu@gmail.com>
2022-09-13 22:43:53 +0100

    13 Sept

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:warehouse/screen/homepage.dart';
 import 'package:warehouse/widgets/homepage/locator.dart';
@@ -12,14 +14,76 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   final _appgreen = const Color.fromRGBO(28, 190, 142, 1);
-  final List _screens = [
-    HomePage(),
-    Container(),
-    Container(),
-    Container(),
-  ];
+  // var didChange = true;
 
-////
+  // @override
+  // void didChangeDependencies() {
+  //   if (didChange) {
+  //     showModalBottomSheet(
+  //         context: context,
+  //         builder: (context) => Container(
+  //               height: 200,
+  //               width: 300,
+  //               color: Colors.amber,
+  //             ));
+  //     didChange = false;
+  //   }
+  //   // TODO: implement didChangeDependencies
+  //   super.didChangeDependencies();
+  // }
+
+  @override
+  void initState() {
+    Timer(
+        Duration(seconds: 1),
+        (() => showModalBottomSheet(
+              context: context,
+              builder: (context) => Container(
+                height: 400,
+                color: Colors.white,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Align(
+                          alignment: Alignment.topCenter,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 220, 217, 217),
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            margin: const EdgeInsets.only(top: 10, bottom: 20),
+                            height: 5,
+                            width: 35,
+                          )),
+                      Container(
+                        margin: const EdgeInsets.only(left: 18),
+                        child: const Text(
+                          'Set up Business Profile',
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 18, vertical: 10),
+                        child: const Text(
+                            'To access business tools we need to provide some ' +
+                                'information about your business',
+                            maxLines: 2,
+                            style: TextStyle(height: 1.6),
+                            softWrap: true),
+                      ),
+
+                      
+                    ]),
+              ),
+            )));
+    // TODO: implement initState
+    super.initState();
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -29,21 +93,15 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    final List _screens = [
+      HomePage(
+        ctx: context,
+      ),
+      Container(),
+      Container(),
+      Container(),
+    ];
     return Scaffold(
-//  Container(
-//             child: Row(children: [
-//               Locator(context1: context),
-//               Expanded(child: Container()),
-      // IconButton(
-      //   onPressed: () {},
-      //   icon: const Icon(Icons.add_circle_outline),
-      // ),
-      // IconButton(
-      //     onPressed: () {},
-      //     icon: const Icon(Icons.notifications_outlined))
-//             ]),
-//           ),
-
       appBar: _selectedIndex == 0
           ? PreferredSize(
               preferredSize: const Size.fromHeight(60),
--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -3,8 +3,24 @@ import 'package:warehouse/widgets/homepage/green_yellow_box.dart';
 
 import '../widgets/homepage/post.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.ctx}) : super(key: key);
+  final BuildContext ctx;
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  // void initState() {
+  //   showModalBottomSheet(
+  //       context: context,
+  //       builder: (context) {
+  //         return Container(height: 200, width: 400, color: Colors.red);
+  //       });
+  //   // TODO: implement initState
+  //   super.initState();
+  // }
 
   //// provider for boxes details  make map for
   final List<String> _list = [
@@ -30,6 +46,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mediaQ = MediaQuery.of(context).size;
+
     return SingleChildScrollView(
       child: Container(
         width: mediaQ.width,
